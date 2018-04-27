901c6183fe8eb8062efc63553f4332be0e7b29eb
Antonio De Marco <antonio@nuvole.org>
2018-04-27 08:57:49 +0200

    OPENEUROPA-557: Initial work.

---
+++ tests/Kernel/SmokeTest.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_multilingual\Kernel;
+
+use Drupal\KernelTests\KernelTestBase;
+
+/**
+ * Class SmokeTest.
+ */
+class SmokeTest extends KernelTestBase {
+
+  /**
+   * Smoke test.
+   */
+  public function testSmokeTest(): void {
+    $this->assertTrue(TRUE);
+  }
+
+}
