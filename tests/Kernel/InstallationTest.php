857a5bb1821495f02e4be3ea73d76933748a7885
Antonio De Marco <antonio@nuvole.org>
2018-05-04 08:37:12 +0200

    OPENEUROPA-557: Adding kernel test.

---
+++ tests/Kernel/InstallationTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\oe_multilingual\Kernel;
+
+use Drupal\administration_language_negotiation\Plugin\LanguageNegotiation\LanguageNegotiationAdministrationLanguage;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\KernelTests\KernelTestBase;
+use Drupal\language\Plugin\LanguageNegotiation\LanguageNegotiationUrl;
+use Drupal\language\Plugin\LanguageNegotiation\LanguageNegotiationSelected;
+
+/**
+ * Class InstallationTest.
+ */
+class InstallationTest extends KernelTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = [
+    'administration_language_negotiation',
+    'content_translation',
+    'locale',
+    'language',
+    'oe_multilingual',
+    'system',
+    'user',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->installConfig([
+      'locale',
+      'language',
+      'content_translation',
+      'administration_language_negotiation',
+      'oe_multilingual',
+    ]);
+    $this->container->get('module_handler')->loadInclude('oe_multilingual', 'install');
+    oe_multilingual_install();
+  }
+
+  /**
+   * Test that configuration is properly setup during module installation.
+   */
+  public function testInstallation(): void {
+    $config = $this->config('locale.settings');
+    $this->assertEquals(FALSE, $config->get('translation.import_enabled'));
+    $this->assertEquals(TRUE, $config->get('translate_english'));
+
+    $config = $this->config('administration_language_negotiation.negotiation');
+    $this->assertEquals('en', $config->get('default_language'));
+    $this->assertEquals([
+      '/admin',
+      '/admin/*',
+      '/node/add/*',
+      '/node/*/edit',
+      '/node/*/translations',
+    ], $config->get('paths'));
+
+    $interface_settings = [
+      LanguageNegotiationAdministrationLanguage::METHOD_ID => -20,
+      LanguageNegotiationUrl::METHOD_ID => -19,
+      LanguageNegotiationSelected::METHOD_ID => 20,
+    ];
+    $content_settings = [
+      LanguageNegotiationUrl::METHOD_ID => -19,
+      LanguageNegotiationSelected::METHOD_ID => 20,
+    ];
+
+    $config = $this->config('language.types');
+    $this->assertEquals([
+      LanguageInterface::TYPE_INTERFACE,
+      LanguageInterface::TYPE_CONTENT,
+    ], $config->get('configurable'));
+    $this->assertEquals($interface_settings, $config->get('negotiation.' . LanguageInterface::TYPE_INTERFACE . '.enabled'));
+    $this->assertEquals($interface_settings, $config->get('negotiation.' . LanguageInterface::TYPE_INTERFACE . '.method_weights'));
+    $this->assertEquals($content_settings, $config->get('negotiation.' . LanguageInterface::TYPE_CONTENT . '.enabled'));
+    $this->assertEquals($content_settings, $config->get('negotiation.' . LanguageInterface::TYPE_CONTENT . '.method_weights'));
+  }
+
+}
