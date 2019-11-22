07f4f46d1102dcd1d6d679fe65ff0097b9f08781
22Alexandra <beldean.ale@gmail.com>
2019-11-22 10:58:46 +0100

    OPENEUROPA-2420: Update kernel test.

--- modules/oe_multilingual_front_page/tests/src/Kernel/PathProcessorFrontPageTest.php
+++ modules/oe_multilingual_front_page/tests/src/Kernel/PathProcessorFrontPageTest.php
@@ -5,6 +5,7 @@ declare(strict_types = 1);
 namespace Drupal\Tests\oe_multilingual_front_page\Unit;
 
 use Drupal\Core\Language\LanguageInterface;
+use Drupal\language\Entity\ConfigurableLanguage;
 use Drupal\Tests\token\Kernel\KernelTestBase;
 use Drupal\Tests\node\Traits\ContentTypeCreationTrait;
 use Drupal\Tests\node\Traits\NodeCreationTrait;
@@ -36,6 +37,10 @@ class PathProcessorFrontPageTest extends KernelTestBase {
     'filter',
     'text',
     'oe_multilingual_front_page',
+    'language',
+    'content_translation',
+    'token',
+    'user',
   ];
 
   /**
@@ -55,8 +60,9 @@ class PathProcessorFrontPageTest extends KernelTestBase {
     $this->installSchema('node', 'node_access');
     $this->installEntitySchema('user');
     $this->installEntitySchema('node');
-    $this->installConfig('node');
-    $this->installConfig('filter');
+    $this->installConfig(['system', 'node', 'filter']);
+
+    ConfigurableLanguage::createFromLangcode('fr')->save();
   }
 
   /**
@@ -68,14 +74,13 @@ class PathProcessorFrontPageTest extends KernelTestBase {
     $this->drupalCreateContentType(['type' => 'article', 'name' => 'Article']);
     $node = $this->drupalCreateNode(['type' => 'article', 'title' => 'Test page']);
     // Set the node as front page.
-    $this->config('system.site')->set('page.front', '/node/1')->save();
+    $this->config('system.site')->set('page.front', '/node/' . $node->id())->save();
     // Set node alias.
     \Drupal::service('path.alias_storage')->save($node->toUrl()->toString(), '/test-page', LanguageInterface::LANGCODE_NOT_SPECIFIED);
-    // Set front page.
     $node_alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $node->id());
     $front_uri = \Drupal::config('system.site')->get('page.front');
     $front_alias = \Drupal::service('path.alias_manager')->getAliasByPath($front_uri);
-    $this->assertEquals($front_alias, $node_alias);
+    $this->assertEquals($front_alias, '/test-page');
 
     // Update node alias.
     $node_alias = \Drupal::service('path.alias_storage')->load(['alias' => $node_alias]);
@@ -83,7 +88,7 @@ class PathProcessorFrontPageTest extends KernelTestBase {
     $node_alias = \Drupal::service('path.alias_manager')->getAliasByPath('/node/' . $node->id());
     $front_uri = \Drupal::config('system.site')->get('page.front');
     $front_alias = \Drupal::service('path.alias_manager')->getAliasByPath($front_uri);
-    $this->assertEquals($front_alias, $node_alias);
+    $this->assertEquals($front_alias, '/new-alias');
 
     // Remove node alias.
     \Drupal::service('path.alias_storage')->delete(['alias' => $node_alias]);
@@ -97,6 +102,18 @@ class PathProcessorFrontPageTest extends KernelTestBase {
     $front_uri = \Drupal::config('system.site')->get('page.front');
     $front_alias = \Drupal::service('path.alias_manager')->getAliasByPath($front_uri);
     $this->assertNotEqual($front_alias, $node_alias);
+
+    // New node translatable node.
+    $node = $this->drupalCreateNode(['type' => 'oe_demo_translatable_page', 'title' => 'Translatable page']);
+    $node->addTranslation('fr', ['title' => 'Translatable page fr']);
+    \Drupal::service('path.alias_storage')->save($node->toUrl()->toString(), '/translatable-page', LanguageInterface::LANGCODE_NOT_SPECIFIED);
+    $this->config('system.site')->set('page.front', '/node/' . $node->id())->save();
+    $front_uri = \Drupal::config('system.site')->get('page.front');
+    $front_alias = \Drupal::service('path.alias_manager')->getAliasByPath($front_uri);
+    $this->assertEquals('/translatable-page', $front_alias);
+    \Drupal::configFactory()->getEditable('system.site')->set('default_langcode', 'fr')->save();
+    $this->assertEquals('/translatable-page', $front_alias);
+
   }
 
 }
